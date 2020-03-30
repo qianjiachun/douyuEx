e64f9299f0985442ab24b1d30c9a00e7ddf3d34c
nws0507 <1090067014@qq.com>
2020-03-30 22:09:12 +0800

    添加app端一键签到接口

--- src/packages/Sign/Sign_Yuba.js
+++ src/packages/Sign/Sign_Yuba.js
@@ -1,48 +1,76 @@
 function initPkg_Sign_Yuba() {
-	signYubaList();
+    signYubaList();
+}
+
+function signYubaFast() {
+    GM_xmlhttpRequest({
+        method: "POST",
+        url: "https://mapi-yuba.douyu.com/wb/v3/fastSign",
+        responseType: "json",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+            "client": "android",
+            "token": dyToken,
+        },
+        onload: function (response) {
+            if (response.response.message == "" && response.response.data != 0) {
+                showMessage("【鱼吧】一键签到成功! 获得经验" + response.response.data, "success");
+                // console.log("【鱼吧】" + group_id + "签到成功! 连续" + response.response.data.count + "天 获得经验" + response.response.data.exp);
+            } else if (response.response.data == 0) {
+                showMessage("【鱼吧】没有7级以上的鱼吧或极速签到已完成", "warning");
+                // console.log("【鱼吧】" + group_id + response.response.message);
+            } else {
+                showMessage("【鱼吧】" + response.response.message, "warning");
+            }
+
+        }
+    });
 }
 
 function signYuba(group_id, t) {
-	GM_xmlhttpRequest({
-		method: "POST",
-		url: "https://yuba.douyu.com/ybapi/topic/sign",
-		data: 'group_id=' + group_id,
-		responseType: "json",
-		headers: {
-		  "Content-Type": "application/x-www-form-urlencoded",
-		  "dy-client": "pc",
-		  "dy-token": t,
-		  'Referer': 'https://yuba.douyu.com/group/' + group_id
-		},
-		onload: function(response) {
-			if (response.response.message == "") {
-				showMessage("【鱼吧】" + group_id + "签到成功! 连续" + response.response.data.count + "天 获得经验" + response.response.data.exp, "success");
-				// console.log("【鱼吧】" + group_id + "签到成功! 连续" + response.response.data.count + "天 获得经验" + response.response.data.exp);
-			} else {
-				showMessage("【鱼吧】" + group_id + response.response.message, "warning");
-				// console.log("【鱼吧】" + group_id + response.response.message);
-			}
-		 
-		}
-	});
+    GM_xmlhttpRequest({
+        method: "POST",
+        url: "https://yuba.douyu.com/ybapi/topic/sign",
+        data: 'group_id=' + group_id,
+        responseType: "json",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+            "dy-client": "pc",
+            "dy-token": t,
+            'Referer': 'https://yuba.douyu.com/group/' + group_id
+        },
+        onload: function (response) {
+            if (response.response.message == "") {
+                showMessage("【鱼吧】" + group_id + "签到成功! 连续" + response.response.data.count + "天 获得经验" + response.response.data.exp, "success");
+                // console.log("【鱼吧】" + group_id + "签到成功! 连续" + response.response.data.count + "天 获得经验" + response.response.data.exp);
+            } else {
+                showMessage("【鱼吧】" + group_id + response.response.message, "warning");
+                // console.log("【鱼吧】" + group_id + response.response.message);
+            }
+
+        }
+    });
 }
 
 function signYubaList() {
-	GM_xmlhttpRequest({
-		method: "GET",
-		url: "https://yuba.douyu.com/wbapi/web/group/myFollow?page=1&limit=999",
-		responseType: "json",
-		headers: {
-		  "Content-Type": "application/x-www-form-urlencoded",
-		  "dy-client": "pc",
-		  "dy-token": dyToken
-		},
-		onload: function(response) {
-			for (let i = 0; i < response.response.data.list.length; i++) {
-				signYuba(response.response.data.list[i].group_id, dyToken);
-			}
-		 
-		}
-	});
-	
+    GM_xmlhttpRequest({
+        method: "GET",
+        url: "https://mapi-yuba.douyu.com/wb/v3/signAggregation?page=1",
+        responseType: "json",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+            "client": "android",
+            "token": dyToken
+        },
+        onload: function (response) {
+            signYubaFast();
+            for (let i = 0; i < response.response.data.list.length; i++) {
+                if (response.response.data.list[i].level < 7) {
+                    signYuba(response.response.data.list[i].group_id, dyToken);
+                }
+            }
+
+        }
+    });
+
 }
