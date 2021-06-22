101ae50fe5ef1763ce1034321d722d5908ebf542
qianjiachun <189964430@qq.com>
2021-06-22 08:33:03 +0800

    修复真实人数失效的BUG

--- src/packages/Update/Update.js
+++ src/packages/Update/Update.js
@@ -1,7 +1,7 @@
 // 版本号
 // 格式 yyyy.MM.dd.**
 // var curVersion = "2020.01.12.01";
-var curVersion = "2021.06.16.02"
+var curVersion = "2021.06.22.01"
 var isNeedUpdate = false
 var lastestVersion = ""
 function initPkg_Update() {
