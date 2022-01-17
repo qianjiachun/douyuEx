5100760a9019343a36a94c40775bdf7433d7b7cc
qianjiachun <189964430@qq.com>
2022-01-18 00:43:20 +0800

    feat: 新增一个每周弹框

---
+++ src/packages/WeeklyPanel/WeeklyPanel.js
@@ -0,0 +1,50 @@
+function initPkg_WeeklyPanel() {
+    if (isShowWeeklyPanel()) {
+        initPkg_WeeklyPanel_Dom();
+        initPkg_WeeklyPanel_Func();
+    }
+}
+
+function initPkg_WeeklyPanel_Dom() {
+	let a = document.createElement("div");
+	a.className = "weeklypanel__panel-wrap";
+	a.innerHTML = `
+		<div class="weeklypanel__panel">
+            <div class="weeklypanel__close">×</div>
+			<div class="weeklypanel__content">
+				<div class="weeklypanel__text">😳项目维护不易，如果觉得DouyuEx有帮助到你的话</div>
+				<div class="weeklypanel__text">请点击下方链接，在右上角点个免费的⭐吧！</div>
+				<div class="weeklypanel__text">以鼓励我长期维护下去，感谢使用😇</div>
+				<div class="weeklypanel__text"><a href="https://github.com/qianjiachun/douyuEx" target="_blank">https://github.com/qianjiachun/douyuEx</a></div>
+				<img style="width: 500px;margin-top:50px;" class="weeklypanel__img" src="https://s4.ax1x.com/2022/01/17/7dLnjs.png"/>
+			</div>
+			
+		</div>
+	`;
+	
+	let b = document.getElementsByClassName("layout-Container")[0];
+	b.insertBefore(a, b.childNodes[0]);
+}
+
+function initPkg_WeeklyPanel_Func() {
+    let weeklyPanel = document.getElementsByClassName("weeklypanel__panel-wrap")[0];
+    document.getElementsByClassName("weeklypanel__close")[0].addEventListener("click", () => {
+        weeklyPanel.style.display = "none";
+    });
+}
+
+function isShowWeeklyPanel() {
+    const LOCAL_NAME = "Ex_WeeklyPanel_NextTime";
+    let tt = new Date().getTime();
+    let nextTime = Number(localStorage.getItem(LOCAL_NAME));
+    if (nextTime) {
+        if (tt >= nextTime) {
+            localStorage.setItem(LOCAL_NAME, tt + 604800000);
+            return true;
+        }
+    } else {
+        localStorage.setItem(LOCAL_NAME, tt + 604800000);
+        return true;
+    }
+    return false;
+}
