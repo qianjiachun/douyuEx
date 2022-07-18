69f3901385a70fe4035cd2b55d98feb378cf4a04
qianjiachun <189964430@qq.com>
2022-07-18 16:52:02 +0800

    feat: update 2022.07.18.01

--- src/packages/DisableCloseJump/DisableCloseJump.js
+++ src/packages/DisableCloseJump/DisableCloseJump.js
@@ -2,7 +2,6 @@ function initPkg_DisableCloseJump_Timer() {
   setInterval(() => {
     let x = document.getElementsByClassName("dy-ModalRadius-close-x");
     if (x.length > 0) {
-      clearInterval(timer_closing);
       x[0].click();
     }
   }, 1000);
