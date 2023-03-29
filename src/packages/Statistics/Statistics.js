6774e1790a104b56a8e84fb8c488c2de0f8753b3
qianjiachun <189964430@qq.com>
2023-03-29 14:08:47 +0800

    fix: 修复百度统计

--- src/packages/Statistics/Statistics.js
+++ src/packages/Statistics/Statistics.js
@@ -2,7 +2,7 @@ var _hmt = _hmt || [];
 
 function initPkg_Statistics() {
   let hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?ef649a5ac4355ceef72a7c848b349182";
+  hm.src = "https://hm.baidu.com/hm.js?4dc4fb0549a56fe03ba53c022b1ff455";
   let s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
 }
