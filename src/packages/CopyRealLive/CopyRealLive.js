c41bd3e91d5ab5aef3f84ed22e09f242035f5f44
qianjiachun <189964430@qq.com>
2023-08-18 23:37:02 +0800

    fix: 修复在没有清晰度的直播间无法复制直播流的bug

--- src/packages/CopyRealLive/CopyRealLive.js
+++ src/packages/CopyRealLive/CopyRealLive.js
@@ -25,39 +25,41 @@ function CopyRealLive_getQn(qnName) {
     return 0;
 }
 
-function initPkg_CopyRealLive_Func() {
+function CopyRealLive_copyUrl(qn) {
+    getRealLive_Douyu(rid, true, false, qn, (lurl) => {
+        if (lurl == "None") {
+            showMessage("房间未开播或其他错误", "error");
+        } else {
+            let str = String(lurl);
+            // GM_setClipboard(String(lurl).replace("https", "http"));
+            GM_setClipboard(str);
+            showMessage("复制成功", "success");
+        }
+    })
+}
 
+function initPkg_CopyRealLive_Func() {
 	document.getElementById("copy-real-live").addEventListener("click", function() {
-        document.querySelectorAll(".tipItem-898596 > ul > li").forEach(item => {
-            if (item.className.includes("selected")) {
-                getRealLive_Douyu(rid, true, false, CopyRealLive_getQn(item.innerText), (lurl) => {
-                    if (lurl == "None") {
-                        showMessage("房间未开播或其他错误", "error");
-                    } else {
-                        let str = String(lurl);
-                        // GM_setClipboard(String(lurl).replace("https", "http"));
-                        GM_setClipboard(str);
-                        showMessage("复制成功", "success");
-                    }
-                })
-            }
-        })
+        if (document.querySelectorAll(".tipItem-898596 > ul > li").length > 0) {
+            document.querySelectorAll(".tipItem-898596 > ul > li").forEach(item => {
+                if (item.className.includes("selected")) {
+                    CopyRealLive_copyUrl(CopyRealLive_getQn(item.innerText));
+                }
+            })
+        } else {
+            CopyRealLive_copyUrl(0);
+        }
     });
     document.getElementsByClassName("Title-header")[0].addEventListener("click", function() {
-        document.querySelectorAll(".tipItem-898596 > ul > li").forEach(item => {
-            if (item.className.includes("selected")) {
-                getRealLive_Douyu(rid, true, false, CopyRealLive_getQn(item.innerText), (lurl) => {
-                    if (lurl == "None") {
-                        showMessage("房间未开播或其他错误", "error");
-                    } else {
-                        let str = String(lurl);
-                        // GM_setClipboard(String(lurl).replace("https", "http"));
-                        GM_setClipboard(str);
-                        showMessage("复制成功", "success");
-                    }
-                })
-            }
-        })
+        if (document.querySelectorAll(".tipItem-898596 > ul > li").length > 0) {
+            document.querySelectorAll(".tipItem-898596 > ul > li").forEach(item => {
+                if (item.className.includes("selected")) {
+                    CopyRealLive_copyUrl(CopyRealLive_getQn(item.innerText));
+                }
+            })
+        } else {
+            CopyRealLive_copyUrl(0);
+        }
     });
 
     let titNode = document.getElementsByClassName("RecommendViewTit-04ebd8");
