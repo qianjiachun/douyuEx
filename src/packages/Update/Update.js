be01353894fdb58e43a86e34f44a48dc60de8a9a
qianjiachun <189964430@qq.com>
2023-11-30 14:10:49 +0800

    update: 2023.11.30.02

--- src/packages/Update/Update.js
+++ src/packages/Update/Update.js
@@ -1,7 +1,7 @@
 // 版本号
 // 格式 yyyy.MM.dd.**
 // var curVersion = "2020.01.12.01";
-var curVersion = "2023.11.30.01"
+var curVersion = "2023.11.30.02"
 var isNeedUpdate = false
 var lastestVersion = ""
 function initPkg_Update() {
