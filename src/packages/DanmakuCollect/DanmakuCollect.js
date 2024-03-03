c8cd99c36f514f81d770d3f3e56d2e8b19332b8e
qianjiachun <189964430@qq.com>
2024-03-03 13:42:58 +0800

    feat: 新增无限弹幕收藏

---
+++ src/packages/DanmakuCollect/DanmakuCollect.js
@@ -0,0 +1,58 @@
+function initPkg_DanmakuCollect() {
+  responseHook((url, text) => {
+    if (url.includes(`bulletscreen/query`)) {
+      let obj = JSON.parse(text);
+      obj.data.list.unshift(...getLocalDanmakuCollect().map(item => {
+        return {
+          content: item.content,
+          type: 2,
+          id: item.id
+        }
+      }));
+      return JSON.stringify(obj);
+    }
+  });
+
+  responseHook((url, text, body) => {
+    if (url.includes(`bulletscreen/add`)) {
+      let obj = JSON.parse(text);
+      if (obj.error == 0) return text;
+      let { content } = JSON.parse(body);
+      addLocalDanmakuCollect(content);
+      obj.msg = "收藏成功，云收藏已达上限，将收藏至本地（由DouyuEx插件实现无限收藏）";
+      return JSON.stringify(obj);
+    }
+  });
+
+  responseHook((url, text, body) => {
+    if (url.includes(`bulletscreen/del`)) {
+      let { id } = JSON.parse(body);
+      delLocalDanmakuCollect(id);
+    }
+  });
+}
+
+function getLocalDanmakuCollect() {
+  let ret = localStorage.getItem("ExSave_DanmakuCollect");
+  try {
+    ret = JSON.parse(ret) || [];
+  } catch (error) {
+    ret = [];
+  }
+  return ret;
+}
+
+function addLocalDanmakuCollect(content) {
+  let ret = getLocalDanmakuCollect();
+  ret.unshift({
+    content,
+    id: new Date().getTime()
+  });
+  localStorage.setItem("ExSave_DanmakuCollect", JSON.stringify(ret));
+}
+
+function delLocalDanmakuCollect(id) {
+  let ret = getLocalDanmakuCollect();
+  localStorage.setItem("ExSave_DanmakuCollect", JSON.stringify(ret.filter(item => item.id !== id)));
+}
+
