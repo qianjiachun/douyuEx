4e26b0184663f18cd8156385139e9675332970e5
qianjiachun <189964430@qq.com>
2024-03-03 13:42:37 +0800

    fix: 兼容response hook代码

--- src/packages/ShowDanmaku/ShowDanmaku.js
+++ src/packages/ShowDanmaku/ShowDanmaku.js
@@ -1,13 +1,8 @@
 function initPkg_ShowDanmaku() {
-    let accessor = Object.getOwnPropertyDescriptor(unsafeWindow.XMLHttpRequest.prototype, 'responseText');
-    Object.defineProperty(unsafeWindow.XMLHttpRequest.prototype, 'responseText', {
-        get: function() {
-            let text = accessor.get.call(this);
-            if (this.responseURL.indexOf("/betard") !== -1) {
-                return text.replace('player_barrage\":0', 'player_barrage\":1');
-            }
-            return text;
-        },
-        configurable: true
+    responseHook((url, text) => {
+        if (url.indexOf("/betard") !== -1) {
+            return text.replace('player_barrage\":0', 'player_barrage\":1');
+        }
+        return text;
     });
 }
