29dd3029680eadb415224526cbfa39c8be1fe1a2
qianjiachun <189964430@qq.com>
2024-03-29 11:44:23 +0800

    feat: 新增屏蔽弹幕图片

---
+++ src/packages/Shield/RemoveDanmakuImage.js
@@ -0,0 +1,45 @@
+let isRemoveDanmakuImage = getLocalIsRemoveDanmakuImage();
+function initPkg_Shield_RemoveDanmakuImage() {
+  const shieldTool = document.getElementsByClassName("ShieldTool-list")[0];
+  shieldTool.insertAdjacentHTML(
+    "beforeend",
+    `
+      <div class="ShieldTool-listItem ${isRemoveDanmakuImage ? "is-checked" : "is-noChecked"}" id="ex-RemoveDanmakuImage">
+          <span class="ShieldTool-checkIcon"></span>
+          <h5 class="ShieldTool-checkText">屏蔽弹幕图片</h5>
+      </div>`
+  );
+  if (isRemoveDanmakuImage) removeDanmakuImage();
+  const dom = document.getElementById("ex-RemoveDanmakuImage");
+  dom.addEventListener("click", () => {
+    isRemoveDanmakuImage = !isRemoveDanmakuImage;
+    if (isRemoveDanmakuImage) {
+      removeDanmakuImage();
+      dom.className = dom.className.replace("is-noChecked", "is-checked");
+    } else {
+      StyleHook_remove("Ex_Style_RemoveDanmakuImage");
+      dom.className = dom.className.replace("is-checked", "is-noChecked");
+    }
+    saveRemoveDanmakuImage();
+  });
+}
+
+function getLocalIsRemoveDanmakuImage() {
+  const ret = localStorage.getItem("ExSave_isRemoveDanmakuImage");
+  return ret ? Number(ret) === 1 : false;
+}
+
+function saveRemoveDanmakuImage() {
+  localStorage.setItem("ExSave_isRemoveDanmakuImage", isRemoveDanmakuImage ? 1 : 0);
+}
+
+function removeDanmakuImage() {
+  StyleHook_set(
+    "Ex_Style_RemoveDanmakuImage",
+    `
+    .danmuItem-31f924 a {
+      display: none !important;
+    }
+  `
+  );
+}
