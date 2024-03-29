21749999769ac7359022026e09dd4bcd1fc5d49c
qianjiachun <189964430@qq.com>
2024-03-29 11:44:13 +0800

    feat: 新增屏蔽弹幕背景

---
+++ src/packages/Shield/RemoveDanmakuBackground.js
@@ -0,0 +1,54 @@
+let isRemoveDanmakuBackground = getLocalIsRemoveDanmakuBackground();
+function initPkg_Shield_RemoveDanmakuBackground() {
+  const shieldTool = document.getElementsByClassName("ShieldTool-list")[0];
+  shieldTool.insertAdjacentHTML(
+    "beforeend",
+    `
+      <div class="ShieldTool-listItem ${isRemoveDanmakuBackground ? "is-checked" : "is-noChecked"}" id="ex-removeDanmakuBackground">
+          <span class="ShieldTool-checkIcon"></span>
+          <h5 class="ShieldTool-checkText">屏蔽弹幕背景</h5>
+      </div>`
+  );
+  if (isRemoveDanmakuBackground) removeDanmakuBackground();
+  const dom = document.getElementById("ex-removeDanmakuBackground");
+  dom.addEventListener("click", () => {
+    isRemoveDanmakuBackground = !isRemoveDanmakuBackground;
+    if (isRemoveDanmakuBackground) {
+      removeDanmakuBackground();
+      dom.className = dom.className.replace("is-noChecked", "is-checked");
+    } else {
+      StyleHook_remove("Ex_Style_RemoveDanmakuBackground");
+      dom.className = dom.className.replace("is-checked", "is-noChecked");
+    }
+    saveRemoveDanmakuBackground();
+  });
+}
+
+function getLocalIsRemoveDanmakuBackground() {
+  const ret = localStorage.getItem("ExSave_isRemoveDanmakuBackground");
+  return ret ? Number(ret) === 1 : false;
+}
+
+function saveRemoveDanmakuBackground() {
+  localStorage.setItem("ExSave_isRemoveDanmakuBackground", isRemoveDanmakuBackground ? 1 : 0);
+}
+
+function removeDanmakuBackground() {
+  StyleHook_set(
+    "Ex_Style_RemoveDanmakuBackground",
+    `
+      .danmuItem-31f924 {
+        background: none !important;
+      }
+      .danmuItem-31f924 div{
+          background: none !important;
+      }
+      .danmuItem-31f924 > img {
+          display: none !important;
+      }
+      .danmuItem-31f924 div > img {
+        display: none !important;
+      }
+  `
+  );
+}
