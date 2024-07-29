6dc558be24be6650635c0b3887edf6789a401325
qianjiachun <189964430@qq.com>
2024-07-29 10:39:43 +0800

    release: 2024.07.29.01

--- src/packages/Update/Update.js
+++ src/packages/Update/Update.js
@@ -1,7 +1,7 @@
 // 版本号
 // 格式 yyyy.MM.dd.**
 // var curVersion = "2020.01.12.01";
-var curVersion = "2024.07.27.01"
+var curVersion = "2024.07.29.01"
 var isNeedUpdate = false
 var lastestVersion = ""
 function initPkg_Update() {
