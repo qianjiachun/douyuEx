2b8e4c97b1f28e1767b13bdc5de7a5aa4cb6519b
qianjiachun <189964430@qq.com>
2024-12-23 09:54:22 +0800

    fix: 开播提醒区分轮播

--- src/packages/LiveTool/LiveNotice/LiveNotice.js
+++ src/packages/LiveTool/LiveNotice/LiveNotice.js
@@ -7,7 +7,8 @@ function initPkg_LiveTool_LiveNotice_Handle(text) {
     if (getType(text) == "rss") {
         let rid = getStrMiddle(text, "rid@=", "/");
         let ss = getStrMiddle(text, "ss@=", "/");
-        if (ss == "1") {
+        let ivl = getStrMiddle(text, "ivl@=", "/"); // 区分轮播，当ivl为1时则为轮播
+        if (ss == "1" && ivl == "0") {
             showMessageWindow("开播提醒", "直播间：" + rid + "开播了，点我签到", () => {
                 signRoom(rid);
             });
