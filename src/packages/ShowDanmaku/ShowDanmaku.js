7812e314a0f4ced4d2fb12b8c9e8d6993b69a71b
qianjiachun <189964430@qq.com>
2025-06-20 11:03:25 +0800

    fix: 修复回复按钮不显示的bug

--- src/packages/ShowDanmaku/ShowDanmaku.js
+++ src/packages/ShowDanmaku/ShowDanmaku.js
@@ -15,7 +15,7 @@ function initPkg_ShowDanmakuOriginAction() {
             // 加一按钮
             newContent = newContent.replace(`if(s&&s.isOpenFireFBComment)`, `if(true)`);
             // 回复按钮
-            newContent = newContent.replace(`if(B&&!this.isFireOpenRank(a))if(parseInt(B,10)&&M&&S>=R&&(!L||L&&I))`, `if(true)if(true) `);
+            newContent = newContent.replace(`if(B&&!this.isFireOpenRank(a))if(parseInt(B,10)&&M&&S>=R&&(!L||L&&P))`, `if(true)if(true) `);
             // 点赞按钮
             newContent = newContent.replace(`else if(1==+Object(m.A)("barrage_praise"))`, `if(true) `);
             return newContent;
