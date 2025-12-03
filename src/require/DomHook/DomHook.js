778e982940938d6c6d99abe2d3ef426e7b1ed1a2
PaTTeeL <PaTTeeL@users.noreply.github.com>
2025-12-03 20:08:07 +0000

    feat: 扩展DomHook的参数类型，支持传入元素对象

--- src/require/DomHook/DomHook.js
+++ src/require/DomHook/DomHook.js
@@ -1,16 +1,20 @@
 class DomHook {
-    constructor(selector, isSubtree, callback, observeAttributes = false) {
-        this.selector = selector;
-        this.isSubtree = isSubtree;
-        let targetNode = document.querySelector(this.selector);
+    constructor(elementOrSelector, isSubtree, callback, observeAttributes = false) {
+        let targetNode = null;
+        if (elementOrSelector instanceof Element) {
+            targetNode = elementOrSelector;
+        } else if (typeof elementOrSelector === "string") {
+            targetNode = document.querySelector(elementOrSelector);
+        }
         if (targetNode == null) {
+            console.warn("DouyuEx DomHook: 目标节点不存在或已销毁", elementOrSelector);
             return;
         }
         let observer = new MutationObserver(function(mutations) {
             callback(mutations);
         });
         this.observer = observer;
-        this.observer.observe(targetNode, { attributes: observeAttributes, childList: true, subtree: this.isSubtree });
+        this.observer.observe(targetNode, { attributes: observeAttributes, childList: true, subtree: isSubtree });
     }
     closeHook() {
         if (this.observer) {
