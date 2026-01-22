6486e6e384d8919b211d34e2d23db1f3f5b99a5e
PaTTeeL <PaTTeeL@Hotmail.com>
2026-01-23 04:05:00 +0800

    refactor: 使用CSS类状态管理重构Refresh_Barrage

--- src/packages/Refresh/Refresh_Barrage.js
+++ src/packages/Refresh/Refresh_Barrage.js
@@ -1,13 +1,8 @@
-let current_barrage_status = 0; // 0没被简化 1被简化
-
 function initPkg_Refresh_Barrage() {
-    Promise.all([
-        gDomObserver.waitForElement('.layout-Player-rank'),
-        gDomObserver.waitForElement('.Barrage-toolbar'),
-    ]).then(([dom_rank, toolbar]) => {
+    gDomObserver.waitForElement('.Barrage-toolbar').then(toolbar => {
         initPkg_Refresh_Barrage_Dom(toolbar);
-        initPkg_Refresh_Barrage_Func(dom_rank, toolbar);
-        initPkg_Refresh_Barrage_Set(dom_rank, toolbar);
+        initPkg_Refresh_Barrage_Func(toolbar);
+        initPkg_Refresh_Barrage_Set();
     });
 }
 
@@ -15,51 +10,47 @@ function initPkg_Refresh_Barrage_Dom(toolbar) {
     if (!toolbar.querySelector(".refresh-barrage")) {
         toolbar.insertAdjacentHTML(
             "afterbegin",
-            `<a class="refresh-barrage" id="refresh-barrage">
-                <svg t="1588051109604" id="refresh-barrage__svg" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="3095" width="16" height="16"><path d="M588.416 516.096L787.2 317.312a54.016 54.016 0 1 0-76.416-76.416L512 439.68 313.216 241.024A54.016 54.016 0 1 0 236.8 317.376l198.784 198.848-198.016 197.888a54.016 54.016 0 1 0 76.416 76.416L512 592.576l197.888 197.952a54.016 54.016 0 1 0 76.416-76.416L588.416 516.096z" fill="#AFAFAF" p-id="3096"></path></svg><i class="Barrage-toolbarIcon"></i><span id="refresh-barrage__text" class="Barrage-toolbarText">前缀</span>
+            `<a class="Barrage-toolbarBtn" id="btn-prefixHidden">
+                <svg t="1588051109604" id="btn-prefixHidden__svg" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="3095" width="16" height="16">
+                    <path d="M588.416 516.096L787.2 317.312a54.016 54.016 0 1 0-76.416-76.416L512 439.68 313.216 241.024A54.016 54.016 0 1 0 236.8 317.376l198.784 198.848-198.016 197.888a54.016 54.016 0 1 0 76.416 76.416L512 592.576l197.888 197.952a54.016 54.016 0 1 0 76.416-76.416L588.416 516.096z" fill="#AFAFAF" p-id="3096"></path>
+                </svg>
+                <i class="Barrage-toolbarIcon"></i>
+                <span class="Barrage-toolbarText" id="btn-prefixHidden__text">前缀</span>
             </a>
-            <a class="refresh-barrage" id="refresh-barrage-frame">
-                <svg t="1588051109604" id="refresh-barrage-frame__svg" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="3095" width="16" height="16"><path d="M512 128 192 448h192v448h256V448h192L512 128z" fill="#AFAFAF" p-id="3096"></path></svg><i class="Barrage-toolbarIcon"></i><span id="refresh-barrage-frame__text" class="Barrage-toolbarText">拉高</span>
+            <a class="Barrage-toolbarBtn" id="btn-rankHidden">
+                <svg t="1588051109604" id="btn-rankHidden__svg" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="3095" width="16" height="16">
+                    <path d="M512 128 192 448h192v448h256V448h192L512 128z" fill="#AFAFAF" p-id="3096"></path>
+                </svg>
+                <i class="Barrage-toolbarIcon"></i>
+                <span class="Barrage-toolbarText" id="btn-rankHidden__text">拉高</span>
             </a>`
         );
     }
 }
 
-function initPkg_Refresh_Barrage_Func(dom_rank, toolbar) {
+function initPkg_Refresh_Barrage_Func(toolbar) {
     toolbar.addEventListener("click", e => {
-        if (e.target.closest("#refresh-barrage")) {
-            if (current_barrage_status == 0) {
+        if (e.target.closest("#btn-prefixHidden")) {
+            if (document.body.classList.contains("is-prefixHidden")) {
+                document.body.classList.remove("is-prefixHidden");
+                saveData_Refresh();
+            } else {
                 PostbirdAlertBox.confirm({
                     'title': '提示',
                     'content': '是否屏蔽弹幕前缀（如粉丝牌、钻粉、贵族等标志）',
                     'okBtn': '确定',
                     'cancelBtn': '取消',
                     'onConfirm': function () {
-                        setRefreshBarrage(toolbar);
+                        document.body.classList.add("is-prefixHidden");
                         saveData_Refresh();
                     },
                     'onCancel': function () {
                     }
                 });
-            } else {
-                cancelRefreshBarrage(toolbar);
-                saveData_Refresh();
             }
-        } else if (e.target.closest("#refresh-barrage-frame")) {
-            let dom_activity = document.getElementById("js-room-activity");
-            let dom_topBarrage = toolbar.closest('.Barrage');
-            if (dom_rank.style.display == "none") {
-                // 被拉高
-                dom_rank.style.display = "block";
-                dom_activity.style.display = "block";
-                dom_topBarrage.className = "Barrage";
-                    toolbar.querySelector("#refresh-barrage-frame").classList.remove("ex-active");
-                toolbar.querySelector("#refresh-barrage-frame__text").style.color = "";
-                let svg = toolbar.querySelector("#refresh-barrage-frame__svg");
-                if (svg) {
-                    let p = svg.getElementsByTagName("path")[0];
-                    if (p) p.setAttribute("fill", "#AFAFAF");
-                }
+        } else if (e.target.closest("#btn-rankHidden")) {
+            if (document.body.classList.contains("is-rankHidden")) {
+                document.body.classList.remove("is-rankHidden");
                 saveData_Refresh();
             } else {
                 PostbirdAlertBox.confirm({
@@ -68,16 +59,7 @@ function initPkg_Refresh_Barrage_Func(dom_rank, toolbar) {
                     'okBtn': '确定',
                     'cancelBtn': '取消',
                     'onConfirm': function () {
-                        dom_rank.style.display = "none";
-                        dom_activity.style.display = "none";
-                        dom_topBarrage.className = "Barrage top-0-important";
-                            toolbar.querySelector("#refresh-barrage-frame").classList.add("ex-active");
-                        toolbar.querySelector("#refresh-barrage-frame__text").style.color = "#fff";
-                        let svg = toolbar.querySelector("#refresh-barrage-frame__svg");
-                        if (svg) {
-                            let p = svg.getElementsByTagName("path")[0];
-                            if (p) p.setAttribute("fill", "#ffffff");
-                        }
+                        document.body.classList.add("is-rankHidden");
                         saveData_Refresh();
                     },
                     'onCancel': function () {
@@ -89,68 +71,22 @@ function initPkg_Refresh_Barrage_Func(dom_rank, toolbar) {
 }
 
 function refresh_Barrage_getStatus() {
-    return current_barrage_status == 1 ? true : false;
+    return document.body.classList.contains("is-prefixHidden");
 }
 
 function refresh_BarrageFrame_getStatus() {
-    let dom_rank = document.getElementsByClassName("layout-Player-rank")[0];
-    return dom_rank ? dom_rank.style.display == "none" : false;
+    return document.body.classList.contains("is-rankHidden");
 }
 
-function initPkg_Refresh_Barrage_Set(dom_rank, toolbar) {
+function initPkg_Refresh_Barrage_Set() {
     let ret = localStorage.getItem("ExSave_Refresh");
     if (ret != null) {
         let retJson = JSON.parse(ret);
-        if ("barrage" in retJson == false) {
-            retJson.barrage = {status: false};
+        if (retJson.barrage && retJson.barrage.status === true) {
+            document.body.classList.add("is-prefixHidden");
         }
-        if (retJson.barrage.status == true) {
-            setRefreshBarrage(toolbar);
+        if (retJson.barrageFrame && retJson.barrageFrame.status === true) {
+            document.body.classList.add("is-rankHidden");
         }
-        if ("barrageFrame" in retJson == false) {
-            retJson.barrageFrame = {status: false};
-        }
-        if (retJson.barrageFrame.status == true) {
-            let dom_activity = document.getElementById("js-room-activity");
-            dom_rank.style.display = "none";
-            dom_activity.style.display = "none";
-            toolbar.querySelector("#refresh-barrage-frame").classList.add("ex-active");
-            toolbar.querySelector("#refresh-barrage-frame__text").style.color = "#fff";
-            let svg = toolbar.querySelector("#refresh-barrage-frame__svg");
-            if (svg) {
-                let p = svg.getElementsByTagName("path")[0];
-                if (p) p.setAttribute("fill", "#ffffff");
-            }
-        }
-    }
-}
- 
-function setRefreshBarrage(toolbar) {
-    let cssText = `
-    .UserCsgoGameDataMedal,.Barrage-honor,.Barrage-listItem .Barrage-icon,.Barrage-listItem .FansMedal.is-made,.Barrage-listItem .RoomLevel,.Barrage-listItem .Motor,.Barrage-listItem .ChatAchievement,.Barrage-listItem .Barrage-hiIcon,.Barrage-listItem .Medal,.Barrage-listItem .MatchSystemTeamMedal{display:none !important;}
-    /*.Barrage-listItem .UserLevel{display:none !important;}*/
-    .Barrage-listItem .Baby{display:none !important;}
-    .FansMedalWrap{display:none !important;}
-    `;
-    StyleHook_set("Ex_Style_RefreshBarrage", cssText);
-    current_barrage_status = 1;
-    toolbar.querySelector("#refresh-barrage").classList.add("ex-active");
-    toolbar.querySelector("#refresh-barrage__text").style.color = "#fff";
-    let svg = toolbar.querySelector("#refresh-barrage__svg");
-    if (svg) {
-        let p = svg.getElementsByTagName("path")[0];
-        if (p) p.setAttribute("fill", "#ffffff");
-    }
-}
-
-function cancelRefreshBarrage(toolbar) {
-    StyleHook_remove("Ex_Style_RefreshBarrage");
-    current_barrage_status = 0;
-    toolbar.querySelector("#refresh-barrage").classList.remove("ex-active");
-    toolbar.querySelector("#refresh-barrage__text").style.color = "";
-    let svg = toolbar.querySelector("#refresh-barrage__svg");
-    if (svg) {
-        let p = svg.getElementsByTagName("path")[0];
-        if (p) p.setAttribute("fill", "#AFAFAF");
     }
 }
