c148870f26e29e51e089a81cdd19de5dcfb68f17
PaTTeeL <PaTTeeL@Hotmail.com>
2026-01-24 23:14:41 +0800

    fix: 优化右侧弹幕区按钮的文案和样式

--- src/packages/Refresh/Refresh_Barrage.js
+++ src/packages/Refresh/Refresh_Barrage.js
@@ -15,14 +15,14 @@ function initPkg_Refresh_Barrage_Dom(toolbar) {
                     <path d="M588.416 516.096L787.2 317.312a54.016 54.016 0 1 0-76.416-76.416L512 439.68 313.216 241.024A54.016 54.016 0 1 0 236.8 317.376l198.784 198.848-198.016 197.888a54.016 54.016 0 1 0 76.416 76.416L512 592.576l197.888 197.952a54.016 54.016 0 1 0 76.416-76.416L588.416 516.096z" fill="#AFAFAF" p-id="3096"></path>
                 </svg>
                 <i class="Barrage-toolbarIcon"></i>
-                <span class="Barrage-toolbarText" id="btn-prefixHidden__text">前缀</span>
+                <span class="Barrage-toolbarText" id="btn-prefixHidden__text">隐藏前缀</span>
             </a>
             <a class="Barrage-toolbarBtn" id="btn-rankHidden">
                 <svg t="1588051109604" id="btn-rankHidden__svg" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="3095" width="16" height="16">
                     <path d="M512 128 192 448h192v448h256V448h192L512 128z" fill="#AFAFAF" p-id="3096"></path>
                 </svg>
                 <i class="Barrage-toolbarIcon"></i>
-                <span class="Barrage-toolbarText" id="btn-rankHidden__text">拉高</span>
+                <span class="Barrage-toolbarText" id="btn-rankHidden__text">隐藏榜单</span>
             </a>`
         );
     }
